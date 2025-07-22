e146ed7f79a92e23753f71faead89cd0341979e3
tanviparab <tanviparab786@gmail.com>
2025-07-22 16:34:52 +0530

    changes in boot application

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer
+ {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
+@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class)
+           }
+		
 }
+
